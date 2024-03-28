635873b040efc77a6d802f34e568d49762285ea6
BuildTools <unconfigured@null.spigotmc.org>
2024-03-28 09:58:10 -0400

    pole

--- TowersOfHanoi/src/Pole.java
+++ TowersOfHanoi/src/Pole.java
@@ -5,7 +5,7 @@ import objectdraw.*;
 public class Pole {
 	
 	FilledRect pole;
-	//
+	
 	public Pole(double x, double y, double wif, double lenf, DrawingCanvas canvas) {
 		
 		pole = new FilledRect(x,y,wif,lenf,canvas);
