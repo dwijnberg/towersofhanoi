bdec02b7f181f3f8d86bc7a0ee6e9c71639a663b
BuildTools <unconfigured@null.spigotmc.org>
2024-03-29 11:19:26 -0400

    add stack

--- TowersOfHanoi/src/Pole.java
+++ TowersOfHanoi/src/Pole.java
@@ -1,15 +1,36 @@
 import java.awt.Color;
+import java.util.Stack;
 
 import objectdraw.*;
 
 public class Pole {
 	
 	FilledRect pole;
+	Stack<Disk> disks;
 	
 	public Pole(double x, double y, double wif, double lenf, DrawingCanvas canvas) {
 		
 		pole = new FilledRect(x,y,wif,lenf,canvas);
 		pole.setColor(Color.black);
+		disks = new Stack<Disk>();
+		
+	}
+	
+	public void addDisk(Disk d) {
+		
+		disks.add(d);
+		
+	}
+	
+	public Disk getDisk() {
+		
+		return disks.lastElement();
+		
+	}
+	
+	public boolean removeDisk(Disk d) {
+		
+		return disks.remove(d);
 		
 	}
 
