6b55e2f24e81d815a04a31d6edefc14d7e7302a6
BuildTools <unconfigured@null.spigotmc.org>
2024-03-29 10:52:09 -0400

    3 poles

--- TowersOfHanoi/src/TowersOfHanoi.java
+++ TowersOfHanoi/src/TowersOfHanoi.java
@@ -5,7 +5,11 @@ public class TowersOfHanoi extends WindowController {
 
 	public void begin() {
 		
-		Pole p1 = new Pole(120, 150, 10, 400, canvas);
+		Pole p1 = new Pole(120, 250, 10, 350, canvas);
+		Pole p2 = new Pole(360, 250, 10, 350, canvas);
+		Pole p3 = new Pole(600, 250, 10, 350, canvas);
+
+
 		
 	}
 	
